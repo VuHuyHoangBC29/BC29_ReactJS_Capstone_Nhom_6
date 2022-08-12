99a085d6b57cc1b1c36d29dd28045458d0682650
VuHuyHoangBC29 <demigodkiller912@gmail.com>
2022-08-12 22:48:55 +0700

    update

--- src/modules/show-times/show-times.jsx
+++ src/modules/show-times/show-times.jsx
@@ -23,6 +23,7 @@ export default function ShowTimes() {
     service: () => fetchMovieShowTimesApi(params.movieId),
   });
 
+  console.log(showTimes);
   // useEffect(() => {
   //   fetchMovieShowTimes();
   // }, []);
